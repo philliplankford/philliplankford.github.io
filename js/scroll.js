97fa30b17914898c9c76bffeeda13362796f6a6f
Phillip <Flip.dlankford@gmail.com>
2021-08-05 00:35:23 -0700

    small redesign new cube nav replaced skills

---
+++ js/scroll.js
@@ -0,0 +1,22 @@
+//const box = element.getBoundingClientRect();
+// box.top box.right box.bottom box.left;
+
+// let LastKnownScrollPosition = 0; 
+// let ticking = false;
+
+// function doSomething(scrollPos) {
+//     // Do something with the scroll position
+//   }
+  
+//   document.addEventListener('scroll', function(e) {
+//     lastKnownScrollPosition = window.scrollY;
+  
+//     if (!ticking) {
+//       window.requestAnimationFrame(function() {
+//         doSomething(lastKnownScrollPosition);
+//         ticking = false;
+//       });
+  
+//       ticking = true;
+//     }
+//   });
