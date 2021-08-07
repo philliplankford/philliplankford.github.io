f79398bd41a247e9198be10bb985e671b95cb507
Phillip <Flip.dlankford@gmail.com>
2021-08-07 14:27:39 -0700

    a tag centering and added new image

--- js/projectDatabase.js
+++ js/projectDatabase.js
@@ -3,54 +3,54 @@ projects = [
     {
         name: "employee directory",
         year: "2021",
-        tags: "searchable employee catalog fetch api",
+        tags: "searchable fetch api",
         description: "View and search an employee directory. Data is fetched from an external API. Click an employee to view detailed information in a modal window.",
         codeLink: "https://github.com/philliplankford/tech-degree-project-8",
-        siteLink: "",
+        siteLink: "https://philliplankford.github.io/tech-degree-project-8/",
         icon: ""
     },
     {
         name: "web app",
         year: "2021",
-        tags: "web app dashboard data library",
-        description: "a mock up of a web app dashboard that displays data traffic and social activity",
+        tags: "dashboard traffic data",
+        description: "A template web app dashboard that displays data traffic and social activity, complete with an alert function and saved settings using local storage.",
         codeLink: "https://github.com/philliplankford/tech-degree-project-7",
+        siteLink: "https://philliplankford.github.io/tech-degree-project-7/",
+        icon: ""
+    },
+    {
+        name: "guess the rapper",
+        year: "2021",
+        tags: "name guessing javascript game",
+        description: "Players guess the names of rappers hangman style. Guess wrong three times and you lose. Javascript is used to monitor the user interaction with the keyboard and fill out corresponding letters in the hidden name.",
+        codeLink: "https://github.com/philliplankford/tech-degree-project-6",
+        siteLink: "https://philliplankford.github.io/tech-degree-project-6/",
+        icon: ""
+    },
+    {
+        name: "photo gallery",
+        year: "2021",
+        tags: "searchable photo gallery",
+        description: "Displays an image gallery using grid. Javascript controls the search functions. Click an image to view a modal displaying the image in full size alongside a caption.",
+        codeLink: "https://github.com/philliplankford/techdegree-project-5",
         siteLink: "",
         icon: ""
     },
     {
-        name: "employee directory",
+        name: "submission form",
         year: "2021",
-        tags: "searchable employee catalog from api",
-        description: "a mock up of a web app dashboard that displays many types of data",
-        codeLink: "",
+        tags: "responsive form",
+        description: "A fully responsive submission form for an email subscription service.",
+        codeLink: "https://github.com/philliplankford/techdegree-project-3",
         siteLink: "",
         icon: ""
     },
     {
-        name: "web app",
+        name: "template portfolio",
         year: "2021",
-        tags: "unique crypto farming sim",
-        description: "a mock up of a web app dashboard that displays many types of data",
-        codeLink: "",
-        siteLink: "",
-        icon: ""
-    },
-    {
-        name: "web app",
-        year: "2021",
-        tags: "unique crypto farming sim",
-        description: "a mock up of a web app dashboard that displays many types of data",
-        codeLink: "",
-        siteLink: "",
-        icon: ""
-    },
-    {
-        name: "web app",
-        year: "2021",
-        tags: "unique crypto farming sim",
-        description: "a mock up of a web app dashboard that displays many types of data",
-        codeLink: "",
+        tags: "responsive",
+        description: "An example portfolio to showcase a students developement projects.",
+        codeLink: "https://github.com/philliplankford/techdegree-project-2",
         siteLink: "",
         icon: ""
     },
