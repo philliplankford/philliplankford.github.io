221543ae6aeaa7b609da88506fef15bd35c45701
Phillip <Flip.dlankford@gmail.com>
2021-08-08 14:25:37 -0700

    database updates

--- js/projectDatabase.js
+++ js/projectDatabase.js
@@ -3,7 +3,7 @@ projects = [
     {
         name: "employee directory",
         year: "2021",
-        tags: "searchable fetch api",
+        tags: "fetch api responsive",
         description: "View and search an employee directory. Data is fetched from an external API. Click an employee to view detailed information in a modal window.",
         codeLink: "https://github.com/philliplankford/tech-degree-project-8",
         siteLink: "https://philliplankford.github.io/tech-degree-project-8/",
@@ -12,7 +12,7 @@ projects = [
     {
         name: "web app",
         year: "2021",
-        tags: "dashboard traffic data",
+        tags: "grid js responsive",
         description: "A template web app dashboard that displays data traffic and social activity, complete with an alert function and saved settings using local storage.",
         codeLink: "https://github.com/philliplankford/tech-degree-project-7",
         siteLink: "https://philliplankford.github.io/tech-degree-project-7/",
@@ -21,8 +21,8 @@ projects = [
     {
         name: "guess the rapper",
         year: "2021",
-        tags: "name guessing javascript game",
-        description: "Players guess the names of rappers hangman style. Guess wrong three times and you lose. Javascript is used to monitor the user interaction with the keyboard and fill out corresponding letters in the hidden name.",
+        tags: "js dom game",
+        description: "Users guess the names of rappers hangman style. Javascript is used to monitor the user interaction with the keyboard and fill out corresponding letters in the hidden name, inserting elements into the DOM via JS.",
         codeLink: "https://github.com/philliplankford/tech-degree-project-6",
         siteLink: "https://philliplankford.github.io/tech-degree-project-6/",
         icon: ""
@@ -30,8 +30,8 @@ projects = [
     {
         name: "photo gallery",
         year: "2021",
-        tags: "searchable photo gallery",
-        description: "Displays an image gallery using grid. Javascript controls the search functions. Click an image to view a modal displaying the image in full size alongside a caption.",
+        tags: "grid js responsive",
+        description: "Displays an image gallery using CSS grid. JavaScript filters the image via the search bar. Click an image to view a modal displaying the image in full size alongside its caption.",
         codeLink: "https://github.com/philliplankford/techdegree-project-5",
         siteLink: "https://philliplankford.github.io/techdegree-project-5/",
         icon: ""
@@ -39,7 +39,7 @@ projects = [
     {
         name: "submission form",
         year: "2021",
-        tags: "responsive form",
+        tags: "form responsive",
         description: "A fully responsive submission form for an email subscription service.",
         codeLink: "https://github.com/philliplankford/techdegree-project-3",
         siteLink: "https://philliplankford.github.io/techdegree-project-3/",
@@ -48,8 +48,8 @@ projects = [
     {
         name: "template portfolio",
         year: "2021",
-        tags: "responsive",
-        description: "An example portfolio to showcase a students developement projects.",
+        tags: "html css responsive",
+        description: "An example portfolio to showcase a students developement projects. Fully responsive.",
         codeLink: "https://github.com/philliplankford/techdegree-project-2",
         siteLink: "https://philliplankford.github.io/techdegree-project-2/",
         icon: ""
